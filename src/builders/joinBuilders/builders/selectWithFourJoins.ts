357bf03e0d21e0f437b7253585e8a114d4aca283
AndriiSherman <sherman@lambda.direct>
2021-08-04 09:51:12 +0300

    Add 4 joins support

--- src/builders/joinBuilders/builders/selectWithFourJoins.ts
+++ src/builders/joinBuilders/builders/selectWithFourJoins.ts
@@ -8,35 +8,28 @@ import { ExtractModel } from '../../../tables/inferTypes';
 import Select from '../../lowLvlBuilders/selects/select';
 import Expr from '../../requestBuilders/where/where';
 import Join from '../join';
-import SelectResponseTwoJoins from '../responses/selectResponseTwoJoins';
+import SelectResponseFourJoins from '../responses/selectResponseFourJoins';
 import AbstractJoined from './abstractJoinBuilder';
-import SelectTRBWithThreeJoins from './selectWithThreeJoins';
 
-export default class SelectTRBWithTwoJoins<COLUMN extends
-ColumnType, T1, T2, MODEL> extends AbstractJoined<MODEL> {
+export default class SelectTRBWithFourJoins<COLUMN extends
+ColumnType, T1, T2, T3, T4, MODEL> extends AbstractJoined<MODEL> {
   private _join1: Join<COLUMN, T1>;
   private _join2: Join<COLUMN, T2>;
+  private _join3: Join<COLUMN, T3>;
+  private _join4: Join<COLUMN, T4>;
 
   public constructor(tableName: string, session: Session,
-    filter: Expr, join1: Join<COLUMN, T1>, join2: Join<COLUMN, T2>,
+    filter: Expr, join1: Join<COLUMN, T1>, join2: Join<COLUMN, T2>, join3: Join<COLUMN, T3>,
+    join4: Join<COLUMN, T4>,
     columns: { [name in keyof ExtractModel<MODEL>]: Column<ColumnType>; }) {
     super(filter, tableName, session, columns);
     this._join1 = join1;
     this._join2 = join2;
+    this._join3 = join3;
+    this._join4 = join4;
   }
 
-  public join = <T3>(join: Join<COLUMN, T3>):
-  SelectTRBWithThreeJoins<COLUMN, T1, T2, T3, MODEL> => new SelectTRBWithThreeJoins(
-    this._tableName,
-    this._session,
-    this._filter,
-    this._join1,
-    this._join2,
-    join,
-    this._columns,
-  );
-
-  public execute = async (): Promise<SelectResponseTwoJoins<MODEL, T1, T2>> => {
+  public execute = async (): Promise<SelectResponseFourJoins<MODEL, T1, T2, T3, T4>> => {
     const queryBuilder = Select.from(this._tableName, Object.values(this._columns));
     if (this._filter) {
       queryBuilder.filteredBy(this._filter);
@@ -56,6 +49,10 @@ ColumnType, T1, T2, MODEL> extends AbstractJoined<MODEL> {
     { [name in keyof ExtractModel<T1>]: Column<ColumnType>; } = this._join1.mappedServiceToDb;
     const parentTwo:
     { [name in keyof ExtractModel<T2>]: Column<ColumnType>; } = this._join2.mappedServiceToDb;
+    const parentThree:
+    { [name in keyof ExtractModel<T3>]: Column<ColumnType>; } = this._join3.mappedServiceToDb;
+    const parentFour:
+    { [name in keyof ExtractModel<T4>]: Column<ColumnType>; } = this._join4.mappedServiceToDb;
 
     const result = await this._session.execute(query);
     if (result.isLeft()) {
@@ -65,8 +62,10 @@ ColumnType, T1, T2, MODEL> extends AbstractJoined<MODEL> {
       const response = QueryResponseMapper.map(this._columns, result.value);
       const objects = QueryResponseMapper.map(parent, result.value);
       const objectsTwo = QueryResponseMapper.map(parentTwo, result.value);
+      const objectsThree = QueryResponseMapper.map(parentThree, result.value);
+      const objectsFour = QueryResponseMapper.map(parentFour, result.value);
 
-      return new SelectResponseTwoJoins(response, objects, objectsTwo);
+      return new SelectResponseFourJoins(response, objects, objectsTwo, objectsThree, objectsFour);
     }
   };
 }
--- src/builders/joinBuilders/builders/selectWithThreeJoins.ts
+++ src/builders/joinBuilders/builders/selectWithThreeJoins.ts
@@ -8,35 +8,38 @@ import { ExtractModel } from '../../../tables/inferTypes';
 import Select from '../../lowLvlBuilders/selects/select';
 import Expr from '../../requestBuilders/where/where';
 import Join from '../join';
-import SelectResponseTwoJoins from '../responses/selectResponseTwoJoins';
+import SelectResponseThreeJoins from '../responses/selectResponseThreeJoins';
 import AbstractJoined from './abstractJoinBuilder';
-import SelectTRBWithThreeJoins from './selectWithThreeJoins';
+import SelectTRBWithFourJoins from './selectWithFourJoins';
 
-export default class SelectTRBWithTwoJoins<COLUMN extends
-ColumnType, T1, T2, MODEL> extends AbstractJoined<MODEL> {
+export default class SelectTRBWithThreeJoins<COLUMN extends
+ColumnType, T1, T2, T3, MODEL> extends AbstractJoined<MODEL> {
   private _join1: Join<COLUMN, T1>;
   private _join2: Join<COLUMN, T2>;
+  private _join3: Join<COLUMN, T3>;
 
   public constructor(tableName: string, session: Session,
-    filter: Expr, join1: Join<COLUMN, T1>, join2: Join<COLUMN, T2>,
+    filter: Expr, join1: Join<COLUMN, T1>, join2: Join<COLUMN, T2>, join3: Join<COLUMN, T3>,
     columns: { [name in keyof ExtractModel<MODEL>]: Column<ColumnType>; }) {
     super(filter, tableName, session, columns);
     this._join1 = join1;
     this._join2 = join2;
+    this._join3 = join3;
   }
 
-  public join = <T3>(join: Join<COLUMN, T3>):
-  SelectTRBWithThreeJoins<COLUMN, T1, T2, T3, MODEL> => new SelectTRBWithThreeJoins(
+  public join = <T4>(join: Join<COLUMN, T4>):
+  SelectTRBWithFourJoins<COLUMN, T1, T2, T3, T4, MODEL> => new SelectTRBWithFourJoins(
     this._tableName,
     this._session,
     this._filter,
     this._join1,
     this._join2,
+    this._join3,
     join,
     this._columns,
   );
 
-  public execute = async (): Promise<SelectResponseTwoJoins<MODEL, T1, T2>> => {
+  public execute = async (): Promise<SelectResponseThreeJoins<MODEL, T1, T2, T3>> => {
     const queryBuilder = Select.from(this._tableName, Object.values(this._columns));
     if (this._filter) {
       queryBuilder.filteredBy(this._filter);
@@ -56,6 +59,8 @@ ColumnType, T1, T2, MODEL> extends AbstractJoined<MODEL> {
     { [name in keyof ExtractModel<T1>]: Column<ColumnType>; } = this._join1.mappedServiceToDb;
     const parentTwo:
     { [name in keyof ExtractModel<T2>]: Column<ColumnType>; } = this._join2.mappedServiceToDb;
+    const parentThree:
+    { [name in keyof ExtractModel<T3>]: Column<ColumnType>; } = this._join3.mappedServiceToDb;
 
     const result = await this._session.execute(query);
     if (result.isLeft()) {
@@ -65,8 +70,9 @@ ColumnType, T1, T2, MODEL> extends AbstractJoined<MODEL> {
       const response = QueryResponseMapper.map(this._columns, result.value);
       const objects = QueryResponseMapper.map(parent, result.value);
       const objectsTwo = QueryResponseMapper.map(parentTwo, result.value);
+      const objectsThree = QueryResponseMapper.map(parentThree, result.value);
 
-      return new SelectResponseTwoJoins(response, objects, objectsTwo);
+      return new SelectResponseThreeJoins(response, objects, objectsTwo, objectsThree);
     }
   };
 }
