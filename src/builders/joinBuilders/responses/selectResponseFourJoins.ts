89d8a0e2c7473eeee4f836e978d4f78cb1e05e0c
AndriiSherman <sherman@lambda.direct>
2021-08-31 16:10:54 +0300

    group

--- src/builders/joinBuilders/responses/selectResponseFourJoins.ts
+++ src/builders/joinBuilders/responses/selectResponseFourJoins.ts
@@ -19,8 +19,13 @@ export default class SelectResponseFourJoins<T1, T2, T3, T4, T5> {
     this._t5 = t5;
   }
 
+  // public groupBy = (key: keyof ExtractModel<T1>) => {
+  //   const objects = this.map((u, c, c1) => ({ u, c, c1 }));
+  //   return this._groupBy(objects, (item) => key[item]);
+  // };
+
   public map = <M>(imac: (t1: ExtractModel<T1>,
-    t2:ExtractModel<T2>,
+    t2: ExtractModel<T2>,
     t3: ExtractModel<T3>,
     t4: ExtractModel<T4>,
     t5: ExtractModel<T5>) => M): Array<M> => {
@@ -30,4 +35,14 @@ export default class SelectResponseFourJoins<T1, T2, T3, T4, T5> {
     }
     return objects;
   };
+
+  // private _groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) => {
+  //   list.reduce((previous, currentItem) => {
+  //     const group = getKey(currentItem);
+  //     // eslint-disable-next-line no-param-reassign
+  //     if (!previous[group]) previous[group] = [];
+  //     previous[group].push(currentItem);
+  //     return previous;
+  //   }, {} as Record<K, T[]>);
+  // };
 }
