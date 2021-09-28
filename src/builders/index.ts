764604475d4bf982617a0a55e2fd62bb9d0de4c9
AndriiSherman <sherman@lambda.direct>
2021-09-28 13:28:00 +0300

    Add import

--- src/builders/index.ts
+++ src/builders/index.ts
@@ -16,5 +16,5 @@ export { default as Select } from './lowLvlBuilders/selects/select';
 export { default as Update } from './lowLvlBuilders/updates/update';
 export { set, combine } from './requestBuilders/updates/static';
 export {
-  eq, inArray as in, and, or, like, greater, greaterEq, less, lessEq,
+  eq, notEq, inArray as in, and, or, like, greater, greaterEq, less, lessEq,
 } from './requestBuilders/where/static';
