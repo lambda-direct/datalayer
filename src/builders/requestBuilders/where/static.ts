6082d076373d57818cb5450f421555af1ab3ab03
AndriiSherman <sherman@lambda.direct>
2021-10-18 12:27:52 +0300

    Add isNull

--- src/builders/requestBuilders/where/static.ts
+++ src/builders/requestBuilders/where/static.ts
@@ -8,6 +8,7 @@ import EqWhere from './eqWhere';
 import Greater from './greater';
 import GreaterEq from './greaterEq';
 import In from './in';
+import IsNull from './isNull';
 import Less from './less';
 import LessEq from './lessEq';
 import Like from './like';
@@ -19,6 +20,9 @@ import Expr from './where';
 // eslint-disable-next-line max-len
 export const eq = <T extends AbstractColumn<ColumnType<any>, boolean, boolean>>(
   left: T, value: ExtractCodeType<T>): Expr => new EqWhere(new Var<T>(left), new Const(value));
+
+export const isNull = <T extends AbstractColumn<ColumnType<any>, boolean, boolean>>(
+  left: T): Expr => new IsNull(new Var<T>(left));
 
 export const notEq = <T extends AbstractColumn<ColumnType<any>, boolean, boolean>>(
   left: T, value: ExtractCodeType<T>): Expr => new NotEqWhere(new Var<T>(left), new Const(value));
